b157331e2a549e8ac6c278cb8a4ca892e385faab
Simon Dann <simon.dann@halesway.co.uk>
2016-10-28 17:10:16 +0100

    Working on content types

--- src/Entities/ContentType.php
+++ src/Entities/ContentType.php
@@ -2,5 +2,58 @@
 
 class ContentType
 {
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var bool
+     */
+    private $enabled = false;
+
+    /**
+     * @var string
+     */
+    private $path = '';
+
+    /**
+     * @var string
+     */
+    private $template = '';
+
+    /**
+     * @var
+     */
+    private $permalink;
+
+    /**
+     * @var array
+     */
+    private $taxonomies = [];
+
+    /**
+     * ContentType constructor.
+     * @param string $name
+     * @param array $settings
+     */
+    public function __construct($name, array $settings)
+    {
+        $this->name = $name;
+
+        $this->path = (isset($settings['path']) ? $settings['path'] : ('_' . $this->name));
+        $this->template = (isset($settings['template']) ? $settings['template'] : $this->name);
+        $this->permalink = (isset($settings['permalink']) ? $settings['permalink'] : ($this->name . '/%slug%.html'));
+        $this->enabled = (isset($settings['enabled']) ? boolval($settings['enabled']) : false);
+
+        if (isset($settings['taxonomies'])) {
+            foreach ($settings['taxonomies'] as $taxonomy) {
+                $this->taxonomies[$taxonomy] = []; // @todo needs a Collection class here for search/filter/order/pagination/etc?
+            }
+        }
+
+        dd($this);
+    }
+
     // ...
 }
