3c37cee4a8f3f6ddb30669a8f32d7545a696d94a
Simon Dann <simon.dann@gmail.com>
2016-11-02 18:16:31 +0000

    Fixing broken kernel scaffold

--- src/Scaffold/kernel.php
+++ src/Scaffold/kernel.php
@@ -1,8 +1,23 @@
 <?php namespace Site;
 
 use Tapestry\Modules\Kernel\KernelInterface;
+use Tapestry\Tapestry;
 
 class SiteKernel implements KernelInterface {
+
+    /**
+     * @var Tapestry
+     */
+    private $tapestry;
+
+    /**
+     * DefaultKernel constructor.
+     * @param Tapestry $tapestry
+     */
+    public function __construct(Tapestry $tapestry)
+    {
+        $this->tapestry = $tapestry;
+    }
 
     public function boot()
     {
