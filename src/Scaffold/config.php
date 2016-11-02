5bfd4fb256c0adce53fd79bc6a3e1ba6b4e83b70
Simon Dann <carbontwelve@users.noreply.github.com>
2016-11-02 10:32:23 +0000

    Sorted out default scaffold

--- src/Scaffold/config.php
+++ src/Scaffold/config.php
@@ -1,3 +1,17 @@
 <?php
 
-return [];
+return [
+    /**
+     * Some example global data, this is available from $this->site(...) in any phtml file
+     */
+    'site' => [
+        'title' => 'Tapestry Scaffold',
+        'description' => 'Basic site scaffold for the Tapestry static site generator.',
+        'author' => 'Some One <some.one@example.com>'
+    ],
+
+    /**
+     * The site kernel to be loaded during site building
+     */
+    'kernel' => \Site\SiteKernel::class,
+];
---
+++ src/Scaffold/kernel.php
@@ -0,0 +1,11 @@
+<?php namespace Site;
+
+use Tapestry\Modules\Kernel\KernelInterface;
+
+class SiteKernel implements KernelInterface {
+
+    public function boot()
+    {
+        // TODO: Implement boot() method.
+    }
+}
