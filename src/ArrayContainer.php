9806f65c6f4165a2694a5ad8199d1ce3e653e7fd
Simon Dann <carbontwelve@users.noreply.github.com>
2016-11-02 13:14:27 +0000

    Moved Iterator funcationality to ArrayContainer

--- src/ArrayContainer.php
+++ src/ArrayContainer.php
@@ -1,9 +1,17 @@
 <?php namespace Tapestry;
 
+use ArrayAccess;
 use Closure;
+use Iterator;
 
-class ArrayContainer implements \ArrayAccess
+class ArrayContainer implements ArrayAccess, Iterator
 {
+    /**
+     * Current array key pointer for foreach
+     * @var int
+     */
+    private $index = 0;
+
     /**
      * Data item array
      * @var array
@@ -242,6 +250,67 @@ class ArrayContainer implements \ArrayAccess
     public function offsetUnset($offset)
     {
         $this->remove($offset);
+    }
+
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        $k = array_keys($this->items);
+        $var = $this->items[$k[$this->index]];
+        return $var;
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        $this->index++;
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        $k = array_keys($this->items);
+        $var = $k[$this->index];
+        return $var;
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        $k = array_keys($this->items);
+        return isset($k[$this->index]);
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        $this->index = 0;
     }
 
     /**
