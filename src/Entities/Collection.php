e917481e9551742bed824f3ef4bdde9dbf5d986b
Simon Dann <simon.dann@halesway.co.uk>
2016-11-02 10:25:43 +0000

    Added blanking ...

--- src/Entities/Collection.php
+++ src/Entities/Collection.php
@@ -3,4 +3,6 @@
 use Tapestry\ArrayContainer;
 
 // do we need search/filter/order/pagination/etc functionality baked into this class?
-class Collection extends ArrayContainer {}
+class Collection extends ArrayContainer {
+    // ...
+}
