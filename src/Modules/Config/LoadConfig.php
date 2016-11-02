841eff888d82d8313515ca9dde8f15ac11a8e74a
Simon Dann <simon.dann@halesway.co.uk>
2016-11-02 11:41:48 +0000

    Changed Step interface

--- src/Modules/Config/LoadConfig.php
+++ src/Modules/Config/LoadConfig.php
@@ -1,32 +1,45 @@
 <?php namespace Tapestry\Modules\Config;
 
+use Symfony\Component\Console\Output\OutputInterface;
 use Tapestry\Entities\Configuration;
 use Tapestry\Entities\Project;
 use Tapestry\Step;
+use Tapestry\Tapestry;
 
 class LoadConfig implements Step
 {
+    /**
+     * @var Tapestry
+     */
+    private $tapestry;
+
+    public function __construct(Tapestry $tapestry)
+    {
+        $this->tapestry = $tapestry;
+    }
+
     /**
      * If configuration exists, load it from the site and merge it with the default configuration for Tapestry The
      * defaults set must exist for Tapestry to function correctly.
      *
      * @param Project $project
-     * @return mixed
+     * @param OutputInterface $output
+     * @return boolean
      */
-    public function __invoke(Project $project)
+    public function __invoke(Project $project, OutputInterface $output)
     {
-        $configPath = $project->get('cwd') . DIRECTORY_SEPARATOR . 'config.php';
+        $configPath = $project->currentWorkingDirectory . DIRECTORY_SEPARATOR . 'config.php';
 
         if (! file_exists($configPath)){
-            $project->getOutput()->writeln('[!] The config file could not be opened at ['.$configPath.']');
+            $output->writeln('[!] The config file could not be opened at ['.$configPath.']');
             return false;
         }
 
         $configuration = new Configuration(include(__DIR__ . DIRECTORY_SEPARATOR . 'DefaultConfig.php'));
         $configuration->merge(include($configPath));
-        $project->set('config', $configuration);
 
-        $project->getOutput()->writeln('Loaded Config From ['. $configPath .']');
+        $this->tapestry->getContainer()->share(Configuration::class, $configuration);
+        $output->writeln('Loaded Config From ['. $configPath .']');
         return true;
     }
 }
--- src/Modules/Kernel/LoadKernel.php
+++ src/Modules/Kernel/LoadKernel.php
@@ -1,5 +1,6 @@
 <?php namespace Tapestry\Modules\Kernel;
 
+use Symfony\Component\Console\Output\OutputInterface;
 use Tapestry\Entities\Configuration;
 use Tapestry\Entities\Project;
 use Tapestry\Step;
@@ -12,28 +13,32 @@ class LoadKernel implements Step
      */
     private $tapestry;
 
-    public function __construct(Tapestry $tapestry)
+    /**
+     * @var Configuration
+     */
+    private $configuration;
+
+    public function __construct(Tapestry $tapestry, Configuration $configuration)
     {
         $this->tapestry = $tapestry;
+        $this->configuration = $configuration;
     }
 
     /**
      * Process the Project at current.
      *
      * @param Project $project
-     * @return mixed
+     * @param OutputInterface $output
+     * @return boolean
      */
-    public function __invoke(Project $project)
+    public function __invoke(Project $project, OutputInterface $output)
     {
-        $kernelPath = $project->get('cwd') . DIRECTORY_SEPARATOR . 'kernel.php';
-
-        /** @var Configuration $configuration */
-        $configuration = $project->get('config');
+        $kernelPath = $project->currentWorkingDirectory . DIRECTORY_SEPARATOR . 'kernel.php';
 
         // Should we warn the user if the kernel.php exists but their configuration is malformed?
         if (file_exists($kernelPath)){
             include $kernelPath;
-            $this->tapestry->getContainer()->share(KernelInterface::class, $configuration->get('kernel', DefaultKernel::class))->withArgument(
+            $this->tapestry->getContainer()->share(KernelInterface::class, $this->configuration->get('kernel', DefaultKernel::class))->withArgument(
                 $this->tapestry->getContainer()->get(Tapestry::class)
             );
         }else{
