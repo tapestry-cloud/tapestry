d38c0b1156653fbe9fc1ce4f7923aaaea56a7586
Simon Dann <simon.dann@halesway.co.uk>
2016-11-02 16:52:40 +0000

    Added clear tmp folder step

--- src/Modules/Content/Clear.php
+++ src/Modules/Content/Clear.php
@@ -1,18 +1,43 @@
 <?php namespace Tapestry\Modules\Content;
 
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Tapestry\Entities\Project;
 use Tapestry\Step;
 
 class Clear implements Step
 {
     /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * Clear constructor.
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    /**
      * Process the Project at current.
      *
      * @param Project $project
+     * @param OutputInterface $output
      * @return mixed
      */
-    public function __invoke(Project $project)
+    public function __invoke(Project $project, OutputInterface $output)
     {
-        // TODO: Implement __invoke() method.
+        $tmpPath = $project->currentWorkingDirectory . DIRECTORY_SEPARATOR . '.tmp';
+        $output->writeln('[+] Clearing tmp folder ['. $tmpPath .']');
+
+        if (file_exists($tmpPath)){
+            $this->filesystem->remove($tmpPath);
+        }
+        $this->filesystem->mkdir($tmpPath);
+
+        return true;
     }
 }
