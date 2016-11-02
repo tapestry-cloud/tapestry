758a382e374fbf1e1b2c71afd5dc71b81ced7866
Simon Dann <simon.dann@halesway.co.uk>
2016-11-02 10:28:43 +0000

    Added source path checking

--- src/Modules/Content/LoadSourceFiles.php
+++ src/Modules/Content/LoadSourceFiles.php
@@ -13,6 +13,13 @@ class LoadSourceFiles implements Step
      */
     public function __invoke(Project $project)
     {
+        $sourcePath = $project->get('cwd') . DIRECTORY_SEPARATOR . 'source';
+
+        if (! file_exists($sourcePath)){
+            $project->getOutput()->writeln('[!] The project source path could not be opened at ['.$sourcePath.']');
+            return false;
+        }
+
         // TODO: Implement __invoke() method.
     }
 }
