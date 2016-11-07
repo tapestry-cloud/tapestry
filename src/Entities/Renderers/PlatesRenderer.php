a2ca241c3e7a16da713bdc4cebc4c2aca09b13c2
Simon Dann <simon.dann@gmail.com>
2016-11-07 14:52:26 +0000

    Working on plates renderer

--- src/Entities/Renderers/PlatesRenderer.php
+++ src/Entities/Renderers/PlatesRenderer.php
@@ -1,13 +1,37 @@
 <?php namespace Tapestry\Entities\Renderers;
 
+use League\Plates\Engine;
 use Tapestry\Entities\File;
+use Tapestry\Entities\Project;
 
 class PlatesRenderer implements RendererInterface
 {
     /**
      * @var array File extensions that this renderer supports
      */
-    private $extensions = ['phtml', 'php'];
+    private $extensions = ['phtml'];
+
+    /**
+     * @var Engine
+     */
+    private $parser;
+
+    /**
+     * @var Project
+     */
+    private $project;
+
+    /**
+     * PlatesRenderer constructor.
+     * @param Engine $parser
+     * @param Project $project
+     */
+    public function __construct(Engine $parser, Project $project)
+    {
+
+        $this->parser = $parser;
+        $this->project = $project;
+    }
 
     /**
      * Returns an array of the extensions that this renderer will support.
@@ -38,7 +62,14 @@ class PlatesRenderer implements RendererInterface
      */
     public function render(File $file)
     {
-        // ... todo
+        $this->parser->setDirectory($this->project->sourceDirectory);
+        $this->parser->setFileExtension('phtml');
+        return $this->parser->render(
+            $file->getFileInfo()->getRelativePath() .
+            DIRECTORY_SEPARATOR .
+            pathinfo($file->getFileInfo()->getFilename(), PATHINFO_FILENAME),
+            $file->getData()
+        );
     }
 
     /**
