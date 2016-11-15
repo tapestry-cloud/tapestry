8e7f9536ae4160c99810c082beeb415e90e3a8ab
Simon Dann <simon.dann@gmail.com>
2016-11-15 14:21:08 +0000

    :art: refactored file system classes

--- src/Entities/Filesystem/FileWriter.php
+++ src/Entities/Filesystem/FileWriter.php
@@ -4,18 +4,30 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Tapestry\Entities\File;
 
-class FileWriter
+class FileWriter implements FilesystemInterface
 {
     /**
      * @var File
      */
     private $file;
+
+    /**
+     * @var string
+     */
     private $destinationPath;
 
     public function __construct(File $file, $destinationPath)
     {
         $this->file = $file;
         $this->destinationPath = $destinationPath;
+    }
+
+    /**
+     * @return File
+     */
+    public function getFile()
+    {
+        return $this->file;
     }
 
     public function __invoke(Filesystem $filesystem, OutputInterface $output)
