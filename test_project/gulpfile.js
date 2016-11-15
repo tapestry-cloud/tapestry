d92cc9d28abbf459d2c87ccfde79e9ec1e86d2c7
Simon Dann <simon.dann@gmail.com>
2016-11-15 09:55:41 +0000

    :sparkles: Added gulp .gitignore and favicon to test_project

---
+++ test_project/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var elixir = require('laravel-elixir');
+var argv = require('yargs').argv;
+
+elixir.config.assetsPath = 'source/_assets';
+elixir.config.publicPath = 'source';
+
+elixir(function (mix) {
+    var env = argv.e || argv.env || 'local';
+    var port = argv.p || argv.port || 3000;
+
+    mix.less('main.less')
+        .exec('tapestry build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
+        .browserSync({
+            port: port,
+            server: {baseDir: 'build_' + env},
+            proxy: null,
+            files: ['build_' + env + '/**/*']
+        });
+});
