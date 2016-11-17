bca6d06cd2b684180c64633578b0c8cf2c385072
Simon Dann <carbontwelve@users.noreply.github.com>
2016-11-17 15:17:15 +0000

    :sparkles: Modifying the Kernel

--- src/Modules/Kernel/KernelInterface.php
+++ src/Modules/Kernel/KernelInterface.php
@@ -2,5 +2,15 @@
 
 interface KernelInterface
 {
+    /**
+     * This method is executed by Tapestry when the Kernel is registered
+     * @return void
+     */
+    public function register();
+
+    /**
+     * This method of executed by Tapestry as part of the build process
+     * @return void
+     */
     public function boot();
 }
