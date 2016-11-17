f3001e47d1730765871672bde431b070fd6a6f34
Simon Dann <simon.dann@halesway.co.uk>
2016-11-17 16:28:40 +0000

    :sparkles: Boilerplating self update

--- src/Console/Commands/SelfUpdateCommand.php
+++ src/Console/Commands/SelfUpdateCommand.php
@@ -1,6 +1,7 @@
 <?php namespace Tapestry\Console\Commands;
 
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
@@ -16,6 +17,11 @@ class SelfUpdateCommand extends Command
      * @var Finder
      */
     private $finder;
+
+    /**
+     * @var string
+     */
+    private $releaseApi = 'https://api.github.com/repos/carbontwelve/tapestry/releases/latest';
 
     /**
      * InitCommand constructor.
@@ -35,11 +41,23 @@ class SelfUpdateCommand extends Command
     protected function configure()
     {
         $this->setName('self-update')
-            ->setDescription('Update your installed version of Tapestry.');
+            ->setDescription('Update your installed version of Tapestry.')
+            ->setDefinition([
+                new InputOption('rollback', 'r', InputOption::VALUE_NONE, 'Revert to an older installation of tapestry'),
+                new InputOption('clean-backups', null, InputOption::VALUE_NONE, 'Delete old backups during an update. This makes the current version of tapestry the only backup available after the update'),
+            ]);
     }
 
     protected function fire()
     {
+        $localFilename = realpath($_SERVER['argv'][0]) ?: $_SERVER['argv'][0];
+        if (pathinfo($localFilename, PATHINFO_EXTENSION) !== 'phar'){
+            $this->output->writeln('[!] Self-Update only works on phar archives.');
+            exit(1);
+        }
+
+        $tempFilename = dirname($localFilename) . '/' . basename($localFilename, '.phar').'-temp.phar';
+        dd($tempFilename);
         return 0;
     }
 }
