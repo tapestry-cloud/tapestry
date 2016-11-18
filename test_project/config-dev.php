ae1c2255aa2470a4c4110c9560512f0534381873
Simon Dann <simon.dann@halesway.co.uk>
2016-11-18 16:16:08 +0000

    :sparkles: Added environment config loading, solves issue #8

---
+++ test_project/config-dev.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    /**
+     * Some example global data, this is available from $this->site(...) in any phtml file
+     */
+    'site' => [
+        'title' => 'Test',
+    ]
+];
