9bd919996c0b4cbe2993f36791126afd4884ccce
Simon Dann <simon.dann@halesway.co.uk>
2016-11-21 10:25:11 +0000

    :sparkles: Moved tapestry bootstrapping to bootstrap.php so it can be included in unit tests

---
+++ src/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!defined('TAPESTRY_START')) {
+    define('TAPESTRY_START', microtime(true));
+}
+
+if (version_compare(phpversion(), '5.4', '<')) {
+    die('You must use PHP >= 5.4 in order to use Tapestry. Please upgrade your PHP version.');
+}
+if (!ini_get('date.timezone')) {
+    date_default_timezone_set('UTC');
+}
+
+setlocale(LC_ALL, 'en_US.UTF8');
+
+// Phar includes
+if (isset($include)) {
+    require_once $include . '/vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require_once __DIR__ . '/../../../autoload.php';
+} else {
+    echo "Please run composer install." . PHP_EOL;
+    exit(1);
+}
+
+$tapestry = new \Tapestry\Tapestry();
+$tapestry->register(\Tapestry\Providers\ProjectConfigurationServiceProvider::class);
+$tapestry->register(\Tapestry\Providers\ProjectKernelServiceProvider::class);
+$tapestry->register(\Tapestry\Providers\CompileStepsServiceProvider::class);
+$tapestry->register(\Tapestry\Providers\CommandServiceProvider::class);
+$tapestry->register(\Tapestry\Providers\PlatesServiceProvider::class);
+
+return $tapestry;
