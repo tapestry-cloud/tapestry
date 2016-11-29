01ebbf23315d5e42f48bf4d7c00c2169b2820943
Simon Dann <carbontwelve@users.noreply.github.com>
2016-11-29 12:54:34 +0000

    :white_check_mark: Adding Unpublish/Publish draft unit test

---
+++ tests/UnPublishDraftsTest.php
@@ -0,0 +1,54 @@
+<?php namespace Tapestry\Tests;
+
+
+class UnPublishDraftsTest extends CommandTestBase
+{
+
+    /**
+     * Test that setting draft to true in a blog posts front matter ensures that it is not published
+     */
+    public function testUnpublishDrafts()
+    {
+        $this->copyDirectory('assets/build_test_12/src', '_tmp');
+
+        $output = $this->runCommand('build', ['--quiet']);
+
+        $this->assertEquals('', trim($output->getDisplay()));
+        $this->assertEquals(0, $output->getStatusCode());
+
+        $this->assertFileEquals(
+            __DIR__ . '/assets/build_test_11/check/blog/2016/test-blog-entry.html',
+            __DIR__ . '/_tmp/build_local/blog/2016/test-blog-entry/index.html',
+            '',
+            true
+        );
+        self::assertFileNotExists(__DIR__ . '/_tmp/build_local/blog/2016/test-blog-entry-two/index.html');
+    }
+
+    /**
+     * Test Configuration
+     */
+    public function testPublishDraftsConfigurationOverride()
+    {
+        $this->copyDirectory('assets/build_test_13/src', '_tmp');
+
+        $output = $this->runCommand('build', ['--quiet']);
+
+        $this->assertEquals('', trim($output->getDisplay()));
+        $this->assertEquals(0, $output->getStatusCode());
+
+        $this->assertFileEquals(
+            __DIR__ . '/assets/build_test_11/check/blog/2016/test-blog-entry.html',
+            __DIR__ . '/_tmp/build_local/blog/2016/test-blog-entry/index.html',
+            '',
+            true
+        );
+
+        $this->assertFileEquals(
+            __DIR__ . '/assets/build_test_11/check/blog/2016/test-blog-entry-two.html',
+            __DIR__ . '/_tmp/build_local/blog/2016/test-blog-entry-two/index.html',
+            '',
+            true
+        );
+    }
+}
