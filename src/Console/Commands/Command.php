4bcd84acf0f7f7f4a0503a84f497e3abf18bdd06
Simon Dann <simon.dann@gmail.com>
2016-11-29 23:01:00 +0000

    :sparkles: Added Stopwatch solving issue #6

--- src/Console/Commands/Command.php
+++ src/Console/Commands/Command.php
@@ -7,9 +7,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 abstract class Command extends SymfonyCommand
 {
     /** @var  InputInterface */
+
     protected $input;
     /** @var  OutputInterface */
     protected $output;
+
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
@@ -19,18 +21,28 @@ abstract class Command extends SymfonyCommand
     {
         $this->input = $input;
         $this->output = $output;
-        return (int) $this->fire();
+        $result = (int)$this->fire();
+
+        if (defined('TAPESTRY_START') === true && $this->input->getOption('stopwatch')) {
+            $stopwatch = round((microtime(true) - TAPESTRY_START), 3);
+            $this->output->writeln('Task complete in: ' . $stopwatch . 's [' . file_size_convert(memory_get_usage(true)) . '/' . file_size_convert(memory_get_peak_usage(true)) . ']');
+        }
+
+        return $result;
     }
+
     protected function info($string)
     {
         $this->output->writeln("<info>{$string}</info>");
     }
+
     protected function error($string)
     {
         $this->output->writeln('<error>[!]</error> ' . $string);
     }
 
-    protected function panic($string, $code = 1){
+    protected function panic($string, $code = 1)
+    {
         $this->error($string);
         exit($code);
     }
