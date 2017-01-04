f62913ab8c962a6ca68d90f92a64a9ba10eb7a59
Simon Dann <simon.dann@gmail.com>
2017-01-04 16:30:48 +0000

    :sparkles: Added ViewFileTrait containing methods shared between the ViewFile class and the Plates view helpers extension

--- src/Plates/Extensions/Helpers.php
+++ src/Plates/Extensions/Helpers.php
@@ -4,19 +4,47 @@ namespace Tapestry\Plates\Extensions;
 
 use League\Plates\Engine;
 use League\Plates\Extension\ExtensionInterface;
+use Tapestry\Entities\File;
+use Tapestry\Entities\ViewFileTrait;
 
 class Helpers implements ExtensionInterface
 {
-    /** @var \Tapestry\Plates\Template */
+
+    use ViewFileTrait;
+
+    /**
+     * @var \Tapestry\Plates\Template
+     */
     public $template;
 
+    /**
+     * @var File
+     */
+    private $file;
+
+    /**
+     * Register extension function.
+     * @param Engine $engine
+     */
     public function register(Engine $engine)
     {
         $engine->registerFunction('getFile', [$this, 'getFile']);
+        $class = new \ReflectionClass(ViewFileTrait::class);
+        foreach($class->getMethods() as $method) {
+            if ($method->name === 'getFile'){ continue; }
+            $engine->registerFunction($method->name, [$this, $method->name]);
+        }
     }
 
+    /**
+     * @return File
+     */
     public function getFile()
     {
-        return $this->template->getFile();
+        if (is_null($this->file)) {
+            $this->file = $this->template->getFile();
+        }
+
+        return $this->file;
     }
 }
