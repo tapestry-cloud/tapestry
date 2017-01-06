68949f44e01d7547ff74f10af31bdfafa5912486
Simon Dann <simon.dann@gmail.com>
2017-01-06 16:43:04 +0000

    :white_check_mark: TTD, added broken unit test for issue #56

---
+++ tests/MarkdownLayoutTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tapestry\Tests;
+
+class MarkdownLayoutTest extends CommandTestBase
+{
+    public function testMarkdownFilesGetRenderedInLayouts()
+    {
+        $this->copyDirectory('assets/build_test_19/src', '_tmp');
+        $output = $this->runCommand('build', ['--quiet']);
+        $this->assertEquals(0, $output->getStatusCode());
+
+        $this->assertFileEquals(
+            __DIR__.'/assets/build_test_19/check/test.html',
+            __DIR__.'/_tmp/build_local/test/index.html',
+            '',
+            true
+        );
+
+    }
+}
---
+++ tests/assets/build_test_19/src/source/test.md
@@ -0,0 +1,6 @@
+---
+layout: page
+title: "Test"
+---
+
+Test Content
