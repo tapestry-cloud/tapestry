98ca9fb6b2b8fbf2c22ca8d7d658ac343b340d4d
Simon Dann <simon.dann@gmail.com>
2017-01-06 15:14:06 +0000

    :white_check_mark: Added unit test for pagination next/previous, needs to test blog post pages now

--- tests/PaginationGeneratorTest.php
+++ tests/PaginationGeneratorTest.php
@@ -13,4 +13,25 @@ class PaginationGeneratorTest extends CommandTestBase
         $this->assertFileExists(__DIR__.'/_tmp/build_local/blog/index.html');
         $this->assertFileExists(__DIR__.'/_tmp/build_local/blog/2/index.html');
     }
+
+    public function testPaginationNextPrevious()
+    {
+        $this->copyDirectory('assets/build_test_17/src', '_tmp');
+        $output = $this->runCommand('build', ['--quiet']);
+        $this->assertEquals(0, $output->getStatusCode());
+
+        $this->assertFileEquals(
+            __DIR__.'/assets/build_test_17/check/1.html',
+            __DIR__.'/_tmp/build_local/blog/index.html',
+            '',
+            true
+        );
+
+        $this->assertFileEquals(
+            __DIR__.'/assets/build_test_17/check/2.html',
+            __DIR__.'/_tmp/build_local/blog/2/index.html',
+            '',
+            true
+        );
+    }
 }
