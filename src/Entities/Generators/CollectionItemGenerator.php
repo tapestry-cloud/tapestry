b79d1fe06e0ceff144d60dc79f577ad78ed904ae
Simon Dann <simon.dann@gmail.com>
2017-01-07 18:48:27 +0000

    :green_heart: Changed functionality of $previous_next to use Pagination class rather than stdClass as per #54. All Tests now pass.

--- src/Entities/Generators/CollectionItemGenerator.php
+++ src/Entities/Generators/CollectionItemGenerator.php
@@ -2,8 +2,8 @@
 
 namespace Tapestry\Entities\Generators;
 
+use Tapestry\Entities\Pagination;
 use Tapestry\Entities\Project;
-use Tapestry\Entities\ViewFile;
 
 class CollectionItemGenerator extends FileGenerator
 {
@@ -25,21 +25,29 @@ class CollectionItemGenerator extends FileGenerator
         $siblings = array_keys($contentType->getFileList('asc'));
         $position = array_search($this->file->getUid(), $siblings);
 
-        $previousNext = new \stdClass();
-        $previousNext->isFirst = ($position === 0);
-        $previousNext->isLast = ($position === (count($siblings) - 1));
-        $previousNext->next = null;
-        $previousNext->previous = null;
+        if (($position === (count($siblings) - 1))) {
+            // If we are the last page, then Pagination will only have two pages (this and the previous one), also there will
+            // be just two files in the items array
 
-        if (! $previousNext->isFirst) {
-            $previousNext->previous = isset($siblings[$position - 1]) ? new ViewFile($project, $siblings[$position - 1]) : null;
+            $pagination = new Pagination($project, [], 2, 2);
+        } elseif ($position === 0) {
+            // If we are the first page, then Pagination will only have two pages (this and the next one), also there will be
+            // just two files in the items array
+
+            $pagination = new Pagination($project, [], 2, 1);
+        } else {
+            // Else this is the middle page of a total of three (previous, this, next)
+
+            $pagination = new Pagination($project, [], 3, 2);
         }
 
-        if (! $previousNext->isLast) {
-            $previousNext->next = isset($siblings[$position + 1]) ? new ViewFile($project, $siblings[$position + 1]) : null;
-        }
+        $pagination->setPreviousNext(
+            (isset($siblings[$position - 1]) ? $siblings[$position - 1] : null),
+            (isset($siblings[$position + 1]) ? $siblings[$position + 1] : null)
+        );
 
-        $newFile->setData(['previous_next' => $previousNext, 'item' => $this->file]);
+        // @todo check to see if 'item' should be set within the view's scope; it feels weird that this is the only generator doing so
+        $newFile->setData(['previous_next' => $pagination, 'item' => $this->file]);
 
         return $newFile;
     }
