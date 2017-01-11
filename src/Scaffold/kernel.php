994fe8df5791fcf7610b65ec7e29fa004287481e
Simon Dann <simon.dann@gmail.com>
2017-01-11 13:04:41 +0000

    :bug: Fixed init project kernel for #27

--- src/Scaffold/kernel.php
+++ src/Scaffold/kernel.php
@@ -22,8 +22,24 @@ class kernel implements KernelInterface
         $this->tapestry = $tapestry;
     }
 
+    /**
+     * This method is executed by Tapestry when the Kernel is registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // ...
+    }
+
+    /**
+     * This method of executed by Tapestry as part of the build process.
+     *
+     * @return void
+     */
     public function boot()
     {
         // ...
     }
+
 }
