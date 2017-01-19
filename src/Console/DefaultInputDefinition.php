b19d3e741b366cf57e1cf0fa51431c9af702d785
Simon Dann <carbontwelve@users.noreply.github.com>
2017-01-19 11:56:51 +0000

    :art: set command to optional argument, given that it is in-fact optional

--- src/Console/DefaultInputDefinition.php
+++ src/Console/DefaultInputDefinition.php
@@ -10,7 +10,7 @@ class DefaultInputDefinition extends InputDefinition {
     public function __construct(array $definition = [])
     {
         $definition = array_merge($definition, [
-            new InputArgument('command', InputArgument::REQUIRED, 'The command to execute'),
+            new InputArgument('command', InputArgument::OPTIONAL, 'The command to execute'),
             new InputOption('--help', '-h', InputOption::VALUE_NONE, 'Display this help message'),
             new InputOption('--quiet', '-q', InputOption::VALUE_NONE, 'Do not output any message'),
             new InputOption('--verbose', '-v|vv|vvv', InputOption::VALUE_NONE, 'Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug'),
@@ -20,7 +20,7 @@ class DefaultInputDefinition extends InputDefinition {
             new InputOption('--no-interaction', '-n', InputOption::VALUE_NONE, 'Do not ask any interactive question'),
 
             new InputOption('--site-dir', null, InputOption::VALUE_REQUIRED, 'The site directory', getcwd()),
-            new InputOption('--dist-dir', null, InputOption::VALUE_REQUIRED, 'The destination directory', getcwd() . DIRECTORY_SEPARATOR . 'local_build'),
+            new InputOption('--dist-dir', null, InputOption::VALUE_OPTIONAL, 'The destination directory'),
             new InputOption('--env', 'e', InputOption::VALUE_REQUIRED, 'Site environment', 'local'),
             new InputOption('--stopwatch', 's', InputOption::VALUE_NONE, 'Time how long the build took'),
         ]);
