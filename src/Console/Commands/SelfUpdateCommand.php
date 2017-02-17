76b9e6d66131521ee9fca66c8afacde985eb0715
Simon Dann <simon.dann@halesway.co.uk>
2017-02-17 09:53:40 +0000

    :art: removed a blank line thats not required

--- src/Console/Commands/SelfUpdateCommand.php
+++ src/Console/Commands/SelfUpdateCommand.php
@@ -45,7 +45,6 @@ class SelfUpdateCommand extends Command
         parent::__construct();
         if (! isset($_SERVER['argv'])) {
             $this->canExecute = false;
-
             return;
         }
         $this->filesystem = $filesystem;
