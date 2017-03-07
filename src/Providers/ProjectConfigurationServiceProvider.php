1f1fd70e7a9a28bce89a00221c15142d9bd3717d
Simon Dann <simon.dann@gmail.com>
2017-03-07 23:32:57 +0000

    :sparkles: added YAML formatted configuration file parsing as per issue #93. All tests pass.

--- src/Providers/ProjectConfigurationServiceProvider.php
+++ src/Providers/ProjectConfigurationServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Tapestry\Providers;
 
+use Symfony\Component\Yaml\Yaml;
 use Tapestry\Tapestry;
 use Tapestry\Entities\Configuration;
 use League\Container\ServiceProvider\AbstractServiceProvider;
@@ -28,19 +29,43 @@ class ProjectConfigurationServiceProvider extends AbstractServiceProvider
 
         /** @var Tapestry $tapestry */
         $tapestry = $container->get(Tapestry::class);
-
         $configuration = new Configuration(include(__DIR__.'/../../src/Modules/Config/DefaultConfig.php'));
 
-        $configPath = $tapestry['currentWorkingDirectory'].DIRECTORY_SEPARATOR.'config.php';
-        if (file_exists($configPath)) {
-            $configuration->merge(include($configPath));
+        if ($baseConfigPathName = $this->identifyConfigurationPath($tapestry['currentWorkingDirectory'])) {
+            $configuration->merge($this->getConfigurationFromPath($baseConfigPathName));
         }
 
-        $configPath = $tapestry['currentWorkingDirectory'].DIRECTORY_SEPARATOR.'config-'.$tapestry['environment'].'.php';
-        if (file_exists($configPath)) {
-            $configuration->merge(include($configPath));
+        if ($envConfigPathName = $this->identifyConfigurationPath($tapestry['currentWorkingDirectory'], $tapestry['environment'])) {
+            $configuration->merge($this->getConfigurationFromPath($envConfigPathName));
         }
 
         $container->share(Configuration::class, $configuration);
+    }
+
+    private function identifyConfigurationPath($configPath, $env = null) {
+        $basePath = $configPath . DIRECTORY_SEPARATOR . 'config'. (is_null($env) ? '' : ('-' . $env));
+        $PHPPath = $basePath . '.php';
+        $YAMLPath = $basePath . '.yaml';
+        $configPHPExists = file_exists($PHPPath);
+        $configYAMLExists = file_exists($YAMLPath);
+        if ($configPHPExists && $configYAMLExists) {
+            throw new \Exception('Configuration can only be either PHP or YAML based, not both.');
+        }
+        if ($configPHPExists) {
+            return $PHPPath;
+        }
+        if ($configYAMLExists) {
+            return $YAMLPath;
+        }
+        return null;
+    }
+
+    private function getConfigurationFromPath($path) {
+        if (strpos($path, 'php') !== false) {
+            return include($path);
+        }
+        if (strpos($path, 'yaml') !== false) {
+            return Yaml::parse(file_get_contents($path));
+        }
     }
 }
