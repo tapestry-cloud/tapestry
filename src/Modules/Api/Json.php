0aacc3b2bca546296663422d95d415516342457e
Simon Dann <simon.dann@gmail.com>
2017-03-08 23:37:41 +0000

    :construction: adding Json step for #92

---
+++ src/Modules/Api/Json.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tapestry\Modules\Api;
+
+use Tapestry\Step;
+use Tapestry\Entities\Project;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Json implements Step
+{
+
+    /**
+     * Process the Project at current.
+     *
+     * @param Project $project
+     * @param OutputInterface $output
+     *
+     * @return bool
+     */
+    public function __invoke(Project $project, OutputInterface $output)
+    {
+        if ($project->get('cmd_options.json') !== true) {
+            return true;
+        }
+
+        $output->writeln('[+] Exporting JSON Blob');
+    }
+}
