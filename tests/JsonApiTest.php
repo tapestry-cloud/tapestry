5d3c4d4d90f4cfa824b6c8e6f72739719c2d4ab5
Simon Dann <simon.dann@gmail.com>
2017-03-09 00:09:34 +0000

    :white_check_mark: added a failing test

--- tests/JsonApiTest.php
+++ tests/JsonApiTest.php
@@ -6,6 +6,8 @@ class JsonApiTest extends CommandTestBase
 {
     public function testJsonBuildFlag()
     {
-        // ...
+        $this->copyDirectory('assets/build_test_1/src', '_tmp');
+        $output = $this->runCommand('build', ['--quiet', '--json']);
+        $this->assertEquals(0, $output->getStatusCode());
     }
 }
