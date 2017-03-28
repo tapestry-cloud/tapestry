e45a330f7422c2ba4b9b810f99507aa97619edc1
Simon Dann <simon.dann@halesway.co.uk>
2017-03-28 15:50:56 +0100

    :bug: fixed copy functionality for #121

---
+++ src/Modules/Content/Copy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tapestry\Modules\Content;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Tapestry\Entities\Configuration;
+use Tapestry\Entities\Project;
+use Tapestry\Step;
+
+class Copy implements Step
+{
+    /**
+     * @var Configuration
+     */
+    private $configuration;
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * Copy constructor.
+     * @param Configuration $configuration
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Configuration $configuration, Filesystem $filesystem)
+    {
+        $this->configuration = $configuration;
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * Process the Project at current.
+     *
+     * @param Project $project
+     * @param OutputInterface $output
+     *
+     * @return bool
+     */
+    public function __invoke(Project $project, OutputInterface $output)
+    {
+        foreach ($this->configuration->get('copy') as $path) {
+            $finder = new Finder();
+            /** @var SplFileInfo $file */
+            foreach ($finder->files()->in($project->sourceDirectory . DIRECTORY_SEPARATOR . $path) as $file) {
+                $inputPath = $project->sourceDirectory .
+                    DIRECTORY_SEPARATOR .
+                    $path .
+                    DIRECTORY_SEPARATOR .
+                    $file->getRelativePathname();
+
+                $outputPath = $project->destinationDirectory .
+                    DIRECTORY_SEPARATOR .
+                    $path .
+                    DIRECTORY_SEPARATOR .
+                    $file->getRelativePathname();
+
+                $output->writeln('[+] Copying Path [' . $inputPath . '] to path [' . $outputPath . ']');
+                $this->filesystem->copy($inputPath, $outputPath);
+            }
+        }
+        return true;
+    }
+}
