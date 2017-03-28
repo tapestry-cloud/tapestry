b590e5eded8a29e1968d688c3c8095fe3963f244
Simon Dann <carbontwelve@users.noreply.github.com>
2017-03-28 10:56:31 +0100

    :white_check_mark: TDD for issue #118

---
+++ tests/KernelTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tapestry\Tests;
+
+use Symfony\Component\Finder\SplFileInfo;
+use Tapestry\Entities\ContentType;
+use Tapestry\Entities\File;
+use Tapestry\Modules\ContentTypes\ContentTypeFactory;
+
+class KernelTest extends CommandTestBase
+{
+    public function testSiteKernelLoading()
+    {
+        // ...
+    }
+}
