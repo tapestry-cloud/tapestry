fd7af30233b6508ccbb25f9db9fc6ff58d46a812
Simon Dann <simon.dann@halesway.co.uk>
2017-03-28 11:55:46 +0100

    :white_check_mark: Broken test for issue #118

--- tests/KernelTest.php
+++ tests/KernelTest.php
@@ -11,6 +11,17 @@ class KernelTest extends CommandTestBase
 {
     public function testSiteKernelLoading()
     {
-        // ...
+        $this->copyDirectory('assets/build_test_28/src', '_tmp');
+        $output = $this->runCommand('build', '--quiet');
+
+        $this->assertEquals('', trim($output->getDisplay()));
+        $this->assertEquals(0, $output->getStatusCode());
+
+        $this->assertFileEquals(
+            __DIR__.'/assets/build_test_28/check/index.html',
+            __DIR__.'/_tmp/build_local/index.html',
+            '',
+            true
+        );
     }
 }
