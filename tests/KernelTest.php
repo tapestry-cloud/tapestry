20f438a7ef1126e8479c099cfc4961369d9015a7
Simon Dann <simon.dann@gmail.com>
2017-03-29 16:45:05 +0100

    :white_check_mark: added test for for build_test_8 as per #133

--- tests/KernelTest.php
+++ tests/KernelTest.php
@@ -9,6 +9,34 @@ use Tapestry\Modules\ContentTypes\ContentTypeFactory;
 
 class KernelTest extends CommandTestBase
 {
+
+    /**
+     * Written for issue #133
+     * @link https://github.com/carbontwelve/tapestry/issues/133
+     */
+    public function testKernelInterfaceMethodsUsed()
+    {
+        $this->copyDirectory('assets/build_test_8/src', '_tmp');
+        $output = $this->runCommand('build', '--quiet');
+
+        $this->assertEquals('', trim($output->getDisplay()));
+        $this->assertEquals(0, $output->getStatusCode());
+
+        $this->assertFileEquals(
+            __DIR__.'/assets/build_test_8/check/boot.html',
+            __DIR__.'/_tmp/build_local/boot.html',
+            '',
+            true
+        );
+
+        $this->assertFileEquals(
+            __DIR__.'/assets/build_test_8/check/register.html',
+            __DIR__.'/_tmp/build_local/register.html',
+            '',
+            true
+        );
+    }
+
     public function testSiteKernelLoading()
     {
         $this->copyDirectory('assets/build_test_28/src', '_tmp');
