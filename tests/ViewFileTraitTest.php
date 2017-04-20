839d5ed368a5a8df992220203fb3364a9ea8cce6
Simon Dann <simon.dann@halesway.co.uk>
2017-04-20 12:05:31 +0100

    :white_check_mark: TDD for #161

--- tests/ViewFileTraitTest.php
+++ tests/ViewFileTraitTest.php
@@ -2,6 +2,12 @@
 
 namespace Tapestry\Tests;
 
+use Symfony\Component\Finder\SplFileInfo;
+use Tapestry\Entities\File;
+use Tapestry\Entities\Project;
+use Tapestry\Entities\ViewFile;
+use Tapestry\Modules\Content\FrontMatter;
+
 class ViewFileTraitTest extends CommandTestBase
 {
     public function testIsDraftHelper()
@@ -23,5 +29,30 @@ class ViewFileTraitTest extends CommandTestBase
             '',
             true
         );
+    }
+
+    /**
+     * Written for issue #161
+     * @link https://github.com/carbontwelve/tapestry/issues/161
+     */
+    public function testExtractHelper()
+    {
+        // $this->copyDirectory('assets/build_test_32/src', '_tmp');
+        // $output = $this->runCommand('build', '--quiet');
+        // $this->assertEquals(0, $output->getStatusCode());
+
+        $project = new Project('', '', 'test');
+
+        $file = new File(new SplFileInfo(__DIR__ . '/mocks/TestExcerptFile.md', '', ''));
+        $frontMatter = new FrontMatter($file->getFileContent());
+        $file->setData($frontMatter->getData());
+        $file->setContent($frontMatter->getContent());
+
+        $viewFile = new ViewFile($project, $file->getUid());
+
+        $n = $viewFile->getExcerpt();
+        $n = $viewFile->getExcerpt(10);
+        $n = $viewFile->getExcerpt(10, '123');
+        $n = 1;
     }
 }
