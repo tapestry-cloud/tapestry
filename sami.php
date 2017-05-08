98a8e222877586a8a24e5192f695a7e3b28c2e82
Simon Dann <simon.dann@halesway.co.uk>
2017-05-08 14:09:15 +0100

    :sparkles: added sami configuration

---
+++ sami.php
@@ -0,0 +1,27 @@
+<?php
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Scaffold')
+    ->in($dir = __DIR__ . '/src');
+
+// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('1.0.*')
+    ->add('development', 'development branch')
+    ->add('master', 'master branch');
+
+return new Sami($iterator, array(
+    'theme' => 'symfony',
+    'versions' => $versions,
+    'title' => 'Tapestry API',
+    'build_dir' => __DIR__ . '/build/docs/%version%',
+    'cache_dir' => __DIR__ . '/build/cache/%version%',
+    'remote_repository' => new GitHubRemoteRepository('symfony/symfony', dirname($dir)),
+    'default_opened_level' => 2,
+));
