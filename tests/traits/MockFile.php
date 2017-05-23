130ab6f3e3877f1fa9db8a720f78e1b479c1d1e2
Simon Dann <carbontwelve@users.noreply.github.com>
2017-05-23 12:35:27 +0100

    :sparkles: moved file mocking into a trait

---
+++ tests/traits/MockFile.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tapestry\Tests\Traits;
+
+use Symfony\Component\Finder\SplFileInfo;
+use Tapestry\Entities\File;
+use Tapestry\Modules\Content\FrontMatter;
+
+trait MockFile {
+    /**
+     * Return a relative path to a file or directory using base directory.
+     * When you set $base to /website and $path to /website/store/library.php
+     * this function will return /store/library.php
+     *
+     * @param   String   $base   A base path used to construct relative path. For example /website
+     * @param   String   $path   A full path to file or directory used to construct relative path. For example /website/store/library.php
+     *
+     * @return  String
+     */
+    protected function getRelativePath($base, $path) {
+        // On windows strip drive letter
+        $base = preg_replace('/^[A-Z]:/i', '', $base);
+        $path = preg_replace('/^[A-Z]:/i', '', $path);
+
+        // Normalise separator
+        $base = str_replace(['/', '\\'], '/', $base);
+        $path = str_replace(['/', '\\'], '/', $path);
+        $separator = '/';
+
+        $base = array_slice(explode($separator, rtrim($base,$separator)),1);
+        $path = array_slice(explode($separator, rtrim($path,$separator)),1);
+
+        return $separator.implode($separator, array_slice($path, count($base)));
+    }
+
+    protected function mockFile($filePath, $base = __DIR__ . '/..')
+    {
+        $base = realpath($base);
+        $file = new File(new SplFileInfo($filePath, $this->getRelativePath($base, $filePath), $this->getRelativePath($base, $filePath)));
+        $frontMatter = new FrontMatter($file->getFileContent());
+        $file->setData($frontMatter->getData());
+        $file->setContent($frontMatter->getContent());
+        $file->getUid(); // Force the file to generate its uid
+        return $file;
+    }
+}
