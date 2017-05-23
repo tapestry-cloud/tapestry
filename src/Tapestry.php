094df35f6de150d56cd6495df01f01279cf34419
Simon Dann <carbontwelve@users.noreply.github.com>
2017-05-23 16:54:10 +0100

    :bookmark: 1.0.8 Release

--- src/Tapestry.php
+++ src/Tapestry.php
@@ -32,7 +32,7 @@ class Tapestry implements ContainerAwareInterface, ArrayAccess
      *
      * @var string
      */
-    const VERSION = '1.0.8-dev';
+    const VERSION = '1.0.8';
 
     /**
      * Storage of data used by --stopwatch flag.
