d8be9f728772a07c1e44258335c56dd502e5f3c8
Simon Dann <simon.dann@gmail.com>
2017-07-07 13:53:37 +0000

    Apply fixes from StyleCI

--- src/Entities/WorkspaceScaffold/Step.php
+++ src/Entities/WorkspaceScaffold/Step.php
@@ -2,8 +2,8 @@
 
 namespace Tapestry\Entities\WorkspaceScaffold;
 
-use Symfony\Component\Console\Output\OutputInterface;
 use Tapestry\Entities\WorkspaceScaffold;
+use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class Step
 {
