df475ed742e1ebce5ad71795742c8c889b9ac2eb
Simon Dann <simon.dann@gmail.com>
2017-09-21 11:53:52 +0100

    :bug: fixed unit tests broken on linux

--- tests/PlatesExtensionsTest.php
+++ tests/PlatesExtensionsTest.php
@@ -16,16 +16,21 @@ class PlatesExtensionsTest extends CommandTestBase
             mkdir(__DIR__ . DIRECTORY_SEPARATOR . '_tmp' . DIRECTORY_SEPARATOR . 'source');
         }
 
-        $viewFile = $this->mockViewFile($this->mockTapestry(__DIR__ . DIRECTORY_SEPARATOR . '_tmp'), __DIR__ . '/mocks/TestEnvironmentFile.phtml', true);
+        // Test with default Tapestry testing env set
+        $viewFile = $this->mockViewFile(
+            $this->mockTapestry(__DIR__ . DIRECTORY_SEPARATOR . '_tmp'),
+            __DIR__ . '/Mocks/TestEnvironmentFile.phtml', true
+        );
         $this->assertEquals('env: testing', $viewFile->getContent());
 
+        // Test with custom env set
         $viewFile = $this->mockViewFile(
             $this->mockTapestry(
                 __DIR__ . DIRECTORY_SEPARATOR . '_tmp',
                 null,
                 'abc123'
             ),
-            __DIR__ . '/mocks/TestEnvironmentFile.phtml', true
+            __DIR__ . '/Mocks/TestEnvironmentFile.phtml', true
         );
         $this->assertEquals('env: abc123', $viewFile->getContent());
     }
