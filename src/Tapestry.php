3459ec7d2d1b94b99051b02a8dd22e52d98c6ae2
Simon Dann <carbontwelve@users.noreply.github.com>
2017-11-23 11:28:09 +0000

    :bookmark: updating version number for release

--- src/Tapestry.php
+++ src/Tapestry.php
@@ -33,7 +33,7 @@ class Tapestry implements ContainerAwareInterface, ArrayAccess
      *
      * @var string
      */
-    const VERSION = '1.0.10';
+    const VERSION = '1.0.11';
 
     /**
      * Storage of data used by --stopwatch flag.
