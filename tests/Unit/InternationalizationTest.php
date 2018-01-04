b09931b4fa682b883a4e5be48f4047ad8634decb
Simon Dann <carbontwelve@users.noreply.github.com>
2018-01-04 00:03:49 +0000

    :white_check_mark: boilerplate TDD

---
+++ tests/Unit/InternationalizationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tapestry\Tests\Feature;
+
+use Tapestry\Tests\CommandTestBase;
+
+class InternationalizationTest extends CommandTestBase
+{
+    /**
+     * Written for issue #256
+     * @version 1.1.0
+     * @link https://github.com/carbontwelve/tapestry/issues/256
+     */
+    public function testLanguageDefinedCorrectlyViaFrontMatter()
+    {
+        // @todo
+    }
+
+    /**
+     * Written for issue #256
+     * @version 1.1.0
+     * @link https://github.com/carbontwelve/tapestry/issues/256
+     */
+    public function testLanguageDefinedCorrectlyViaPath()
+    {
+        // @todo
+    }
+
+    /**
+     * Written for issue #256
+     * @version 1.1.0
+     * @link https://github.com/carbontwelve/tapestry/issues/256
+     */
+    public function testLanguageDefinedCorrectlyViaFrontMatterWithinPath()
+    {
+        // @todo have a file within a language path define its language in front matter, with the later overriding the former
+    }
+}
