d42b727640d5a14da8eb03c9b59341010a0e02be
Simon Dann <carbontwelve@users.noreply.github.com>
2018-01-17 22:56:35 +0000

    :white_check_mark: boilerplate TDD

--- tests/Unit/InternationalizationTest.php
+++ tests/Unit/InternationalizationTest.php
@@ -6,6 +6,17 @@ use Tapestry\Tests\CommandTestBase;
 
 class InternationalizationTest extends CommandTestBase
 {
+
+    /**
+     * Written for issue #256
+     * @version 1.1.0
+     * @link https://github.com/carbontwelve/tapestry/issues/256
+     */
+    public function testLoadSourceFilesSetsDefaultFileLanguageFromConfig()
+    {
+        // @todo
+    }
+
     /**
      * Written for issue #256
      * @version 1.1.0
