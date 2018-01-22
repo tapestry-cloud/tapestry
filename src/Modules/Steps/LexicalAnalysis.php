f32c6b162cef58cc22002d8e015619b8538986ce
Simon Dann <carbontwelve@users.noreply.github.com>
2018-01-22 13:32:00 +0000

    :truck: added two new steps for #297 in new folder as per #282

---
+++ src/Modules/Steps/LexicalAnalysis.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tapestry\Steps;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Tapestry\Entities\Project;
+use Tapestry\Step;
+
+class LexicalAnalysis implements Step
+{
+
+    /**
+     * Process the Project at current.
+     *
+     * @param Project $project
+     * @param OutputInterface $output
+     *
+     * @return bool
+     */
+    public function __invoke(Project $project, OutputInterface $output)
+    {
+        return true;
+    }
+}
