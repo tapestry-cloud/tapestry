b25d106a28d3cd3fbb3f1b0764763a1faad13086
Simon Dann <simon.dann@gmail.com>
2018-02-21 13:33:15 +0000

    Apply fixes from StyleCI

--- sami-config.php
+++ sami-config.php
@@ -1,17 +1,15 @@
 <?php
 
-use Sami\RemoteRepository\GitHubRemoteRepository;
 use Sami\Sami;
 use Sami\Version\GitVersionCollection;
+use Sami\RemoteRepository\GitHubRemoteRepository;
 
-
-$dir = realpath(__DIR__ .'\src');
+$dir = realpath(__DIR__.'\src');
 
 $versions = GitVersionCollection::create($dir)
     ->addFromTags('1.0.*')
     ->add('2.0.0-dev', '2.0.0 dev-branch')
-    ->add('master', 'master branch')
-;
+    ->add('master', 'master branch');
 
 return new Sami($dir, [
     'versions' => $versions,
@@ -19,5 +17,5 @@ return new Sami($dir, [
     'build_dir' => __DIR__.'/build/docs/%version%',
     'cache_dir' => __DIR__.'/build/cache/%version%',
     'remote_repository' => new GitHubRemoteRepository('tapestry-cloud/tapestry', dirname($dir)),
-    'default_opened_level' => 2
+    'default_opened_level' => 2,
 ]);
