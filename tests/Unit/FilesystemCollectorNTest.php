477232841b59587b0b50d41db89acaf4b218cb43
Simon Dann <carbontwelve@users.noreply.github.com>
2018-02-24 22:38:13 +0000

    Include available mutators for testing

--- tests/Unit/FilesystemCollectorNTest.php
+++ tests/Unit/FilesystemCollectorNTest.php
@@ -3,6 +3,10 @@
 namespace Tapestry\Tests\Unit;
 
 use Tapestry\Modules\Collectors\FilesystemCollector;
+use Tapestry\Modules\Collectors\Mutators\FrontMatterMutator;
+use Tapestry\Modules\Collectors\Mutators\IsDraftMutator;
+use Tapestry\Modules\Collectors\Mutators\IsIgnoredMutator;
+use Tapestry\Modules\Collectors\Mutators\SetDateDataFromFileNameMutator;
 use Tapestry\Tests\TestCase;
 
 class FilesystemCollectorNTest extends TestCase
@@ -21,11 +25,18 @@ class FilesystemCollectorNTest extends TestCase
 
     public function testFilesystemCollector()
     {
-
         $this->loadToTmp($this->assetPath('build_test_41/src'));
 
         try {
-            $class = new FilesystemCollector($this->assetPath('build_test_41/src/source'));
+            $class = new FilesystemCollector(
+                $this->assetPath('build_test_41/src/source'),
+                [
+                    new SetDateDataFromFileNameMutator(),
+                    new FrontMatterMutator(),
+                    new IsDraftMutator(),
+                    new IsIgnoredMutator(),
+                ]
+            );
         } catch (\Exception $e) {
             $this->fail($e);
             return;
@@ -33,6 +44,7 @@ class FilesystemCollectorNTest extends TestCase
 
         $arr = $class->collect();
         $this->assertTrue(is_array($arr));
+        $this->assertCount(10, $arr);
 
     }
 
