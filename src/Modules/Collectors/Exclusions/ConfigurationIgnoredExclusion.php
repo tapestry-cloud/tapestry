e6cb56988ba2e9819fd3bd693bed17213bbdc9c9
Simon Dann <carbontwelve@users.noreply.github.com>
2018-03-01 00:03:35 +0000

    Extended array path exclusion that takes the array input from configured ignores

---
+++ src/Modules/Collectors/Exclusions/ConfigurationIgnoredExclusion.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tapestry\Modules\Collectors\Exclusions;
+
+use Tapestry\Entities\Configuration;
+
+/**
+ * Class ConfigurationIgnoredExclusion
+ *
+ * Filters out files with a path matching any set within the ignored configuration array.
+ *
+ * @package Tapestry\Modules\Collectors\Exclusions
+ */
+class ConfigurationIgnoredExclusion extends ArrayPathExclusion implements ExclusionInterface
+{
+    /**
+     * DraftsExclusion constructor.
+     *
+     * @param Configuration $configuration
+     */
+    public function __construct(Configuration $configuration)
+    {
+        parent::__construct($configuration->get('ignored', []));
+    }
+}
