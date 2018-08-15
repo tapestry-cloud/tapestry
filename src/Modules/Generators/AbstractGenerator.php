b35081224c76cf3fbbb23ffcfff169320eb7adf3
Simon Dann <carbontwelve@users.noreply.github.com>
2018-08-15 23:55:56 +0100

    Added abstract generator for #315

---
+++ src/Modules/Generators/AbstractGenerator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tapestry\Modules\Generators;
+
+use Tapestry\Entities\Project;
+use Tapestry\Modules\Source\SourceInterface;
+
+abstract class AbstractGenerator implements GeneratorInterface
+{
+    /**
+     * @var SourceInterface
+     */
+    protected $source;
+
+    /**
+     * Set the source that this generator works from.
+     *
+     * @param SourceInterface $source
+     * @return void
+     */
+    public function setSource(SourceInterface $source)
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * Run the generation and return an array of generated
+     * files (oddly implementing SourceInterface, naming
+     * things is hard!)
+     *
+     * @param Project $project
+     * @return array|SourceInterface[]
+     */
+    abstract public function generate(Project $project): array;
+}
