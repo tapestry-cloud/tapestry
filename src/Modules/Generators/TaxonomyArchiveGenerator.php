ad1f3dae01354c130e1028a5cfaa4819c01dab2f
Simon Dann <carbontwelve@users.noreply.github.com>
2018-08-16 00:21:48 +0100

    Added generator stubs #315

---
+++ src/Modules/Generators/TaxonomyArchiveGenerator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tapestry\Modules\Generators;
+
+use Tapestry\Entities\Project;
+use Tapestry\Modules\Source\SourceInterface;
+
+class TaxonomyArchiveGenerator extends AbstractGenerator implements GeneratorInterface
+{
+    /**
+     * Run the generation and return an array of generated
+     * files (oddly implementing SourceInterface, naming
+     * things is hard!)
+     *
+     * @param Project $project
+     * @return array|SourceInterface[]
+     */
+    public function generate(Project $project): array
+    {
+        // TODO: Implement generate() method.
+    }
+}
